b06b7b8d158ac604b64f1f97c89919dc0f7bb51f
JessicaLais <142358374+JessicaLais@users.noreply.github.com>
2023-08-19 11:04:02 -0300

    Add files via upload

---
+++ aula2/vetor_matriz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    int v[10];
+    *v = 1;
+
+    printf("%d", v[5]);
+
+    int *v1 = {5, 11, 3};
+    int v2[3];
+    v2[0] = 5;
+    v2[1] = 11;
+    v2[2] = 3;
+
+    for(int i = 0, int i < sizeof[v1]/sizeof ,i++ ){
+        v1[i] > v1[i+1];
+    }
+
+return 0;
+}
